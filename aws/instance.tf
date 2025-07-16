7d1b622aff6ca3924f28f084c8b8868b49166236
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-16 17:44:57 +0900

    fix: subnet_idの指定を修正.

--- aws/instance.tf
+++ aws/instance.tf
@@ -2,7 +2,7 @@ module "bastion" {
   source                      = "./modules/instance/ec2"
   name                        = "live-fire-exercise-bastion"
   vpc_id                      = module.vpc.id
-  subnet_id                   = module.public_subnets[0].id
+  subnet_id                   = module.public_subnets.ids[0]
   associate_public_ip_address = true
   public_key                  = "~/.ssh/live-fire-exercise-bastion.pub"
   ingress = {
