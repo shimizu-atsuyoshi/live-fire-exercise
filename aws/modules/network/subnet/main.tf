4d1a6b670ad80630a5f381e9be9b7a0e6a2a60cf
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-16 15:55:55 +0900

    fix: for_eachの構文エラーを修正.

--- aws/modules/network/subnet/main.tf
+++ aws/modules/network/subnet/main.tf
@@ -18,7 +18,7 @@ variable "map_public_ip_on_launch" {
 }
 
 resource "aws_subnet" "this" {
-  for_each = toset(var.subnets)
+  for_each = { for i in var.subnets : i.name => i }
 
   vpc_id = var.vpc_id
   cidr_block = each.value.cidr_block
