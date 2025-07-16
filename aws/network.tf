b4a0dc38b04047fb62383253abf75a4e2cd2f281
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-16 16:05:48 +0900

    create: インターネットゲートウェイを作成.

--- aws/network.tf
+++ aws/network.tf
@@ -8,9 +8,9 @@ module "public_subnets" {
   source = "./modules/network/subnet"
   vpc_id = module.vpc.id
   subnets = [
-    { name = "public_subnet_a", cidr_block = var.public_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
-    { name = "public_subnet_c", cidr_block = var.public_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
-    { name = "public_subnet_d", cidr_block = var.public_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
+    { name = "live-fire-exercise-public-subnet-a", cidr_block = var.public_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
+    { name = "live-fire-exercise-public-subnet-c", cidr_block = var.public_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
+    { name = "live-fire-exercise-public-subnet-d", cidr_block = var.public_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
   ]
   map_public_ip_on_launch = true
 }
@@ -19,9 +19,15 @@ module "private_subnets" {
   source = "./modules/network/subnet"
   vpc_id = module.vpc.id
   subnets = [
-    { name = "private_subnet_a", cidr_block = var.private_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
-    { name = "private_subnet_c", cidr_block = var.private_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
-    { name = "private_subnet_d", cidr_block = var.private_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
+    { name = "live-fire-exercise-private-subnet-a", cidr_block = var.private_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
+    { name = "live-fire-exercise-private-subnet-c", cidr_block = var.private_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
+    { name = "live-fire-exercise-private-subnet-d", cidr_block = var.private_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
   ]
   map_public_ip_on_launch = false
+}
+
+module "internet_gateway" {
+  source = "./modules/network/internet_gateway"
+  vpc_id = module.vpc.id
+  name = "live-fire-exercise"
 }
