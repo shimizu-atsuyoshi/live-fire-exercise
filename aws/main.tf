e1d67af3dfb561870b53483c22b695ecad14b862
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-16 12:17:53 +0900

    create: リポジトリを初期化.

---
+++ aws/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
