f61f0bb6e00e039f70c2445da8e9d5fa5e1a4ad0
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-16 16:33:34 +0900

    remove: ルートテーブルのtosetを削除

--- aws/modules/network/route_table/main.tf
+++ aws/modules/network/route_table/main.tf
@@ -26,7 +26,7 @@ resource "aws_route_table" "this" {
 }
 
 resource "aws_route_table_association" "this" {
-  for_each = toset(var.subnet_ids)
+  for_each = var.subnet_ids
   subnet_id = each.value
   route_table_id = aws_route_table.this.id
 }
