a4958a01f6e601fa98c6480139bef3ee66241c69
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-16 15:59:05 +0900

    fix: パブリックIP割り当ての指定を修正.

--- aws/modules/network/subnet/main.tf
+++ aws/modules/network/subnet/main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "this" {
   vpc_id = var.vpc_id
   cidr_block = each.value.cidr_block
   availability_zone = each.value.availability_zone
-  map_public_ip_on_launch = each.value.map_public_ip_on_launch
+  map_public_ip_on_launch = var.map_public_ip_on_launch
   tags = {
     "Name" = each.value.name
   }
