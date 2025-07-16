c9eb0c359698c3bf005d2a517fa40bc798cba148
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-16 15:49:19 +0900

    fix: 引数の変数名を修正.

--- aws/network.tf
+++ aws/network.tf
@@ -8,9 +8,9 @@ module "public_subnets" {
   source = "./modules/network/subnet"
   vpc_id = module.vpc.id
   subnets = [
-    { name = "public_subnet_a", cidr = var.public_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
-    { name = "public_subnet_c", cidr = var.public_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
-    { name = "public_subnet_d", cidr = var.public_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
+    { name = "public_subnet_a", cidr_block = var.public_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
+    { name = "public_subnet_c", cidr_block = var.public_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
+    { name = "public_subnet_d", cidr_block = var.public_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
   ]
   map_public_ip_on_launch = true
 }
@@ -19,9 +19,9 @@ module "private_subnets" {
   source = "./modules/network/subnet"
   vpc_id = module.vpc.id
   subnets = [
-    { name = "private_subnet_a", cidr = var.private_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
-    { name = "private_subnet_c", cidr = var.private_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
-    { name = "private_subnet_d", cidr = var.private_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
+    { name = "private_subnet_a", cidr_block = var.private_subnet_a_cidr_block, availability_zone = "ap-northeast-1a" },
+    { name = "private_subnet_c", cidr_block = var.private_subnet_c_cidr_block, availability_zone = "ap-northeast-1c" },
+    { name = "private_subnet_d", cidr_block = var.private_subnet_d_cidr_block, availability_zone = "ap-northeast-1d" },
   ]
   map_public_ip_on_launch = false
 }
