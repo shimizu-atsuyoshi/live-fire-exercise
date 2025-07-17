47b460c5b7aa362d6c186c0eb9908cf383fa96b5
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-07-17 19:10:40 +0900

    update: rdsの接続許可ルールを変更.

--- aws/modules/instance/rds/main.tf
+++ aws/modules/instance/rds/main.tf
@@ -55,6 +55,12 @@ resource "aws_security_group" "this" {
     protocol        = var.ingress.protocol
     security_groups = var.ingress.security_groups
   }
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
