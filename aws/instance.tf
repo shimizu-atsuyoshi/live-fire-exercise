06cdec4302ae310201843d828753367c3685df5f
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-10-06 12:20:10 +0900

    RDSリソースを作成.

--- aws/instance.tf
+++ aws/instance.tf
@@ -1,22 +1,8 @@
-module "bastion" {
-  source                      = "./modules/instance/ec2"
-  name                        = "live-fire-exercise-bastion"
-  vpc_id                      = module.vpc.id
-  subnet_id                   = module.private_subnets.ids[0]
-  associate_public_ip_address = true
-  ingress = {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = []
-  }
-}
-
-module "source_db" {
+module "ai_ci_db" {
   source             = "./modules/instance/rds"
   vpc_id             = module.vpc.id
   subnet_ids         = module.public_subnets.ids
-  cluster_identifier = "source-cluster"
+  cluster_identifier = "ai-ci-cluster"
   master_password    = var.database_master_password
   database_name      = var.database_name
   ingress = {
@@ -29,11 +15,11 @@ module "source_db" {
   }
 }
 
-module "destination_db" {
+module "bin_db" {
   source             = "./modules/instance/rds"
   vpc_id             = module.vpc.id
   subnet_ids         = module.public_subnets.ids
-  cluster_identifier = "destination-cluster"
+  cluster_identifier = "bin-cluster"
   master_password    = var.database_master_password
   database_name      = var.database_name
   ingress = {
@@ -43,28 +29,5 @@ module "destination_db" {
     security_groups = [
       module.dms.security_group_id,
     ]
-  }
-}
-
-module "dms" {
-  source                  = "./modules/instance/dms"
-  vpc_id                  = module.vpc.id
-  subnet_ids              = module.private_subnets.ids
-  replication_instance_id = "live-fire-exercise"
-  source_db = {
-    engine_name   = "aurora"
-    username      = module.source_db.master_username
-    password      = var.database_master_password
-    server_name   = module.source_db.endpoint
-    port          = module.source_db.port
-    database_name = module.source_db.database_name
-  }
-  target_db = {
-    engine_name   = "aurora"
-    username      = module.destination_db.master_username
-    password      = var.database_master_password
-    server_name   = module.destination_db.endpoint
-    port          = module.destination_db.port
-    database_name = module.destination_db.database_name
   }
 }
