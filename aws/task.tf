2a214568c7a41f2d51708a75453117caaabfed2b
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2025-11-27 17:30:25 +0900

    create: sampleのDMSタスクを作成.

--- aws/task.tf
+++ aws/task.tf
@@ -7,3 +7,13 @@ module "store_products" {
   table_mappings            = file("${path.module}/table_mappings/store_products.json")
   replication_task_settings = file("${path.module}/replication_task_settings.json")
 }
+
+module "sample" {
+  source = "./modules/task/dms"
+  replication_task_id       = "sample"
+  replication_instance_arn  = module.dms.replication_instance_arn
+  source_endpoint_arn       = module.dms.source_endpoint_arn
+  target_endpoint_arn       = module.dms.target_endpoint_arn
+  table_mappings            = file("${path.module}/table_mappings/sample.json")
+  replication_task_settings = file("${path.module}/replication_task_settings.json")
+}
