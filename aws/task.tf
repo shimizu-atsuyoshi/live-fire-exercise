533aa8c2766f11814ecb59a04dbdcc570c90a3e2
shimizu-atsuyoshi <shimizu-atsuyoshi@dmm.com>
2026-03-12 14:57:18 +0900

    update: DMSタスクを変更.

--- aws/task.tf
+++ aws/task.tf
@@ -1,10 +1,10 @@
-module "store_products" {
+module "actress_ref" {
   source = "./modules/task/dms"
   replication_task_id       = "store-products"
   replication_instance_arn  = module.dms.replication_instance_arn
   source_endpoint_arn       = module.dms.source_endpoint_arn
   target_endpoint_arn       = module.dms.target_endpoint_arn
-  table_mappings            = file("${path.module}/table_mappings/store_products.json")
+  table_mappings            = file("${path.module}/table_mappings/actress_ref.json")
   replication_task_settings = file("${path.module}/replication_task_settings.json")
 }
 
